66c66b3054081ecb1361c6985ac0c134d7b7678b
3a@hw <arbuzov.aa@inbox.ru>
2024-09-27 14:13:44 +0300

    add: fixture, eslint, settings for __dirname, makefile

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,23 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 import reverse from '../src/index.js';
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFixtureFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test('reverse', () => {
   expect(reverse('hello')).toEqual('olleh');
   expect(reverse('')).toEqual('');
+});
+
+test('reverse with log text', () => {
+  const text = readFixtureFile('dom.html').trim();
+  const expected = reverse(text);
+
+  expect(reverse(text)).toEqual(expected);
 });
